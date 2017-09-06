908d204bde1a0ddf1e65d10bd22bc204c74127c8
Sofia <sofia.img@gmail.com>
2017-09-06 20:33:20 -0300

    Rev 1

--- ex1.rb
+++ ex1.rb
@@ -1,3 +1,45 @@
+def average(filename)
+
+  notas = File.open(filename, 'r').readlines
+
+  notas.map do |nota|
+    a = nota.split(', ').map(&:chomp)
+    b = a[1..5].delete_if { |a| a == "A" }
+    grades = b.map(&:to_f)
+    suma = grades.inject(0) { |sum, g| sum + g }
+    avg = suma / grades.length
+
+    file = File.open('promedios.txt', 'a')
+    file.puts "#{a[0]},  promedio #{avg}"
+  end
+end
+
+def total_absence(filename)
+  file = File.open(filename, 'r')
+  notas = file.readlines
+  file.close
+
+  notas.each do |nota|
+  abs = nota.split(", ").map(&:chomp)
+  print abs[0] + "\t", abs.count('A').to_s + "\n" if abs.include?('A')
+  puts " "
+  end
+  puts " "
+end
+
+def aprobados ()
+file = File.open('notas.csv', 'r')
+notas = file.readlines
+file.close
+
+notas.each do |nota|
+  approved = nota.split(", ").map(&:chomp)
+ print approved[0], "\t", promedio, "\n" if approved >= 5
+end
+end
+
+
+puts 'Registro de notas'
 option = 0
 while option != 4
   puts 'ingresa 1 para mostrar el promedio de notas'
@@ -7,7 +49,7 @@ while option != 4
 
   option = gets.chomp.to_i
 
-  puts case option
+ case option
        when 1
          average('notas.csv')
        when 2
